2aebd841cef7af2831d720bf1c0707d531bd5503
Zayaan AR <zayaan.rajput@gmail.com>
2025-07-21 15:09:10 +0100

    idk misc stuff fr

--- src/assets/scripts/pages/baseUI.js
+++ src/assets/scripts/pages/baseUI.js
@@ -243,7 +243,7 @@ window.loadCompleted = new Promise((resolve) => {
 
                     <hr class="w-12 border-t-2 border-gray-300 dark:border-gray-600 my-1"/>
 
-                    <button onclick="document.documentElement.classList.toggle('dark')" class="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-xl transition z-10 mt-6" style="align-self: center;" title="Toggle themes">
+                    <button id="themeButton" class="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-xl transition z-10 mt-6" style="align-self: center;" title="Toggle themes">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w-6 h-6 text-black dark:text-white" fill="currentColor">
                             <path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/>
                         </svg>
@@ -278,7 +278,7 @@ window.loadCompleted = new Promise((resolve) => {
 
                             <hr class="w-12 border-t-2 border-gray-300 dark:border-gray-600 my-1"/>
 
-                            <button onclick="document.documentElement.classList.toggle('dark')" class="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-xl transition z-10 mt-6" style="align-self: center;" title="Toggle themes">
+                            <button id="themeButton" class="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-xl transition z-10 mt-6" style="align-self: center;" title="Toggle themes">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w-6 h-6 text-black dark:text-white" fill="currentColor">
                                     <path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/>
                                 </svg>
@@ -442,6 +442,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const calendarButton = document.getElementById("calendarButton");
     const newsButton = document.getElementById("newsButton");
     const settingsButton = document.getElementById("settingsButton");
+    const githubButton = document.getElementById("githubButton");
+    const themeButton = document.getElementById("themeButton");
+    const logOutButton = document.getElementById("logOutButton");
 
     homeButton.addEventListener("click", () => { location.pathname = "/" });
     subjectsButton.addEventListener("click", () => { location.pathname = "/subjects" });
@@ -451,4 +454,18 @@ document.addEventListener("DOMContentLoaded", function() {
     calendarButton.addEventListener("click", () => { location.pathname = "/calendar" });
     newsButton.addEventListener("click", () => { location.pathname = "/news" });
     settingsButton.addEventListener("click", () => { location.pathname = "/settings" });
+    githubButton.addEventListener("click", () => { location.href = `https://github.com/userandaname/OpenVLE` });
+    themeButton.addEventListener("click", () => { document.documentElement.classList.toggle('dark'); });
+
+    logOutButton.addEventListener("click", async () => {
+        await contactAPI("https://bromcomvle.com/Auth/Logout", dataHeaders)
+        .then(async blob => {
+            const decodedHTML = (await blob.text()).trim();
+            const parser = new DOMParser();
+            return parser.parseFromString(decodedHTML, "text/html");
+        })
+        .then(bromcom => {
+            location.pathname = "/";
+        })
+    });
 });
